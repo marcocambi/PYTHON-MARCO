a0a9c36a26afc4a3176110af4e7b2249c7d069a8
marcocambi <marco.cambi2010@gmail.com>
2026-08-31 10:28:05 +0000

    Aggiunto codice, generata heatmap e aggiornato README

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Raccolta di script in Python, analisi di dati finanziari e prototipi di ricerca 
 ## Struttura della Repository
 - `src/` o file principali: Script di analisi e visualizzazione dati (es. `stock_heatmap.py`).
 - Sviluppato per testare librerie di calcolo numerico e visualizzazione (`pandas`, `numpy`, `matplotlib`).
+### Anteprima Heatmap
+![Stock Correlation Heatmap](src/heatmap.png)
 
 ## Obiettivo
 Costruire una base solida di codice pulito, modulare e orientato alla finanza quantitativa in vista di progetti di ricerca accademica avanzata.
--- src/stock_heatmap.py
+++ src/stock_heatmap.py
@@ -1,1 +1,36 @@
+import yfinance as yf
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
+#1. Prezzi di chiusura
+tickers = ["AAPL", "MSFT", "TSLA", "GOOGL", "AMZN", "NVDA", "AMD", "META", "JPM", "SPY", "QQQ", "AVGO"]
+data = yf.download(tickers, start = "2020-01-01", end = "2026-01-01", auto_adjust=True)["Close"]
+
+# 2. Calcolo dei rendimenti e della matrice di correlazione
+returns = data.pct_change().dropna()
+corr_matrix = returns.corr()
+
+# 3. Elimina il triangolo superiore
+# mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
+
+# 4. Rendering visivo della Heatmap
+plt.figure(figsize=(12, 10))
+sns.heatmap(
+    corr_matrix, 
+    annot=True, 
+    cmap="coolwarm", 
+    vmin=-1, 
+    vmax=1, 
+    fmt=".2f",
+   # mask=mask,
+    annot_kws={"size": 9}
+)
+
+plt.title("Stock Return Correlation Heatmap", fontsize=14, pad=12)
+plt.tight_layout()
+
+# Invece di plt.show(), salva il grafico come immagine
+plt.savefig("heatmap.png", dpi=300)
+print("Grafico salvato con successo!")
+plt.show
